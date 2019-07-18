f23df59bba8d5faae1fbc7992661d92ccd33e5b8
Michele Ferracin <miferrac@microsoft.com>
2019-07-18 06:43:27 +0200

    Add project files.

---
+++ IC6.RedSvc/Controllers/RandomObjectController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IC6.RedSvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomObjectController : ControllerBase
+    {
+        string[] redObjects = { "Ferrari", "Coca Cola", "Pendolino", "Cherry", "Strawberry" };
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return redObjects[DateTime.Now.Ticks % redObjects.Length];
+        }
+
+        //fault inhection
+
+        //resiliency
+
+        //retry pattern
+    }
+}

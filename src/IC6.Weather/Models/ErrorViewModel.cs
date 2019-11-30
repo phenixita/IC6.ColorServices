4b9b58018743afd604862d47c775210ce85c3052
Michele Ferracin <miferrac@microsoft.com>
2019-11-30 19:04:32 +0100

    Restyle da std

---
+++ src/IC6.Weather/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IC6.Weather.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
